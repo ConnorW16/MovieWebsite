00ab84f4fc0715d6133e2beeee479f17ff74652c
Connor <waltonconnor1@gmail.com>
2024-07-09 20:41:34 +0100

    Update selectedShow.js

--- js/selectedShow.js
+++ js/selectedShow.js
@@ -39,7 +39,7 @@ function findContent(){
         method: 'GET',
         headers: {
           accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkN2ZhZjZhMjdjMmI2M2Y4YWZjOGZiNjA5ZWJhMDU4ZSIsInN1YiI6IjY1Mjk0ZjA5MWYzZTYwMDExYzQ5OTQxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DOlj0BgYx9vWzHficVnFBwSL95hxE4R6FnKDxm7BzHY'
+          Authorization: 'Bearer'
         }
       };
       fetch('https://api.themoviedb.org/3/tv/'+seriesID+'?language=en-US', options)
